b099756224ff69d44d8bc91cc34d7d8bff27526d
Petr Blaha <petrbla@gmail.com>
2022-09-16 09:43:12 +0200

    Create Stockavailabilities.php

---
+++ Stockavailabilities.php
@@ -0,0 +1,15 @@
+$tokenn = token();
+//dotaz na vsechny produkty
+
+$url = "https://api.malfini.com/api/v4/product/availabilities";
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+$headers = array("Accept: application/json","Authorization: Bearer $tokenn",);
+curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+$resp = curl_exec($curl);
+curl_close($curl);
+
+var_dump($resp);
